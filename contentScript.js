23404f0243b70513e3b0cd3c5c84e41f1d9d4632
ombrahmbhatt <ompbarot9816@gmail.com>
2023-05-22 23:08:44 +0530

    id : restructured code to  vanila javascript

--- contentScript.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === "getText") {
-    var selectedText = window.getSelection().toString();
-    chrome.runtime.sendMessage({ action: "updateText", text: selectedText });
-  }
-});
